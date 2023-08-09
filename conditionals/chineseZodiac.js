372ee371373ffd9ba8423da98f27024025ccc216
Георги Бургазлиев <nanobat6@gmail.com>
2023-08-09 21:32:30 +0300

    add new-solution(chineseZodiac)-conditionals

--- conditionals/chineseZodiac.js
+++ conditionals/chineseZodiac.js
@@ -1,40 +1,24 @@
 function chineseZodiac(input) {
   const year = Number(input[0]);
-  if (year % 12 == 2000 % 12) {
-    console.log('Dragon');
-  }
-  if (year % 12 == 2001 % 12) {
-    console.log('Snake');
-  }
-  if (year % 12 == 2002 % 12) {
-    console.log('Horse');
-  }
-  if (year % 12 == 2003 % 12) {
-    console.log('Sheep');
-  }
-  if (year % 12 == 2004 % 12) {
-    console.log('Monkey');
-  }
-  if (year % 12 == 2005 % 12) {
-    console.log('Rooster');
-  }
-  if (year % 12 == 2006 % 12) {
-    console.log('Dog');
-  }
-  if (year % 12 == 2007 % 12) {
-    console.log('Pig');
-  }
-  if (year % 12 == 2008 % 12) {
-    console.log('Rat');
-  }
-  if (year % 12 == 2009 % 12) {
-    console.log('Ox');
-  }
-  if (year % 12 == 2010 % 12) {
-    console.log('Tiger');
-  }
-  if (year % 12 == 2011 % 12) {
-    console.log('Hare');
+  const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  for (let i = 0; i <= arr.length; i++) {
+    if (year % 12 == arr[i]) {
+      switch (year % 12 + '') {
+        case '0': console.log('Monkey'); break;
+        case '1': console.log('Rooster'); break;
+        case '2': console.log('Dog'); break;
+        case '3': console.log('Pig'); break;
+        case '4': console.log('Rat'); break;
+        case '5': console.log('Ox'); break;
+        case '6': console.log('Tiger'); break;
+        case '7': console.log('Hare'); break;
+        case '8': console.log('Dragon'); break;
+        case '9': console.log('Snake'); break;
+        case '10': console.log('Horse'); break;
+        case '11': console.log('Sheep'); break;
+        default: return;
+      }
+    }
   }
 }
 chineseZodiac(['1975']);
