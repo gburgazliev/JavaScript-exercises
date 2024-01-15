044f4fa175757609f500cb75dc39eeb75a083b2d
Georgi Burgazliev <burgazlievgeorgi@gmail.com>
2024-01-15 12:45:44 +0200

    add

--- arrays/arraySearch2.js
+++ arrays/arraySearch2.js
@@ -1,7 +1,7 @@
 let input = ['1,1,1,1,1,1,1,1'];
 let print = this.print || console.log;
 let gets = this.gets || ((arr, index) => () => arr[index++])(input, 0);
-let list = gets().split(',').map(Number).sort();
+const list = gets().split(',').map(Number).sort();
 let newArr = [];
 for (let i = 0; i <= list.length; i++) {
   if (!list.includes(i)) {
